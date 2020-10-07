bd8d3f9a297b678d65011b86b68dfd825ae3b306
paul <paul@pop-os.localdomain>
2020-10-07 15:24:49 +0200

    début page infos

---
+++ montmirail/src/pages/infos.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function OnePage() {
+  return <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+}
--- montmirail/src/pages/site.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import {graphql} from "gatsby";
-import styles from "./index.module.css"
-
-export default function Site({data}){
-
-}
